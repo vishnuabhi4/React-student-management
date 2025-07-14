1798f64c421e6d93bf15bf2238652f4644c30df2
Vishnu.V <110452768+vishnuabhi4@users.noreply.github.com>
2025-07-14 09:47:50 +0530

    Update StudentListPage.jsx

--- src/pages/StudentListPage.jsx
+++ src/pages/StudentListPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useStudents } from '../context/StudentContext'; 
+import { useStudents } from '../context/studentContext'; 
 import StudentList from '../components/StudentList';
 
 const StudentListPage = () => {
